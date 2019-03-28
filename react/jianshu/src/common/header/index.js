8f131535e5b1b329f5f1615431b8e62f1b594f22
shawnxiao <597035529@qq.com>
2019-03-28 10:07:53 +0800

    complelte header

--- react/jianshu/src/common/header/index.js
+++ react/jianshu/src/common/header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { connect } from 'react-redux';
+import { actionCreators } from './store';
 
 import { 
   HeaderWrapper,
@@ -52,23 +53,17 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.focused
+    focused: state.getIn(['header', 'focused'])
   }
 }
 const mapDispatchToProps = (dispatch) => {
   return {
     handleInputFocus () {
-      const action = {
-        type: 'search_focus',
-      };
-      dispatch(action)
+      dispatch(actionCreators.searchFocus())
     },
   
     handleInputBlur () {
-      const action = {
-        type: 'search_blur',
-      };
-      dispatch(action)
+      dispatch(actionCreators.searchBlur())
     }
   }
 }
--- react/jianshu/src/store/reducer.js
+++ react/jianshu/src/store/reducer.js
@@ -1,10 +1,8 @@
-// 合并reducer笔记插件,数据格式直接为不可更改的immutab格式
 import { combineReducers } from 'redux-immutable';
-
 import { reducer as headerReducer } from '../common/header/store';
 
 const reducer = combineReducers({
-  header: headerReducer
-}) 
+	header: headerReducer
+});
 
 export default reducer;
