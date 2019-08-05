39addb89b2a1d3dbd39c4ae33b03dcea2bd41366
shawnxiao <597035529@qq.com>
2019-08-05 15:22:18 +0800

    test eject

--- react/react-js-atd/src/index.js
+++ react/react-js-atd/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import TodoList from './TodoList';
-import '/src/mock';
+import '@/mock';
 
 ReactDom.render(<TodoList />, document.getElementById('root'))
